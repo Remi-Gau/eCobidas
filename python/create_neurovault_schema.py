51e693bc1d2d9d359624beafad975374f02da160
Remi Gau <remi_gau@hotmail.com>
2019-10-23 11:36:14 +0200

    apply pep8

--- python/create_neurovault_schema.py
+++ python/create_neurovault_schema.py
@@ -3,6 +3,11 @@
 
 # tested with python 3.7
 
+import json
+import os
+import csv
+
+
 ## -----------------------------------------------------------------------------
 ## -----------------------------------------------------------------------------
 ## -----------------------------------------------------------------------------
@@ -11,62 +16,57 @@
 # where the metadata from neurovault are described. It is in xlsx folder of this repos
 # but it can also be downloaded from here:
 # https://github.com/NeuroVault/NeuroVault/blob/master/scripts/metadata_neurovault.csv
-input_file = '/home/remi/github/COBIDAS_chckls/xlsx/metadata_neurovault.csv'
+INPUT_FILE = '/home/remi/github/COBIDAS_chckls/xlsx/metadata_neurovault.csv'
 
 # where the files will be written on your machine: the local repository
 # corresponding to the remote where of the schema-standardization will be hosted
-output_dir = '/home/remi/github/schema-standardization'
+OUTPUT_DIR = '/home/remi/github/schema-standardization'
 
 # Placeholder to insert in all instances of the remote repo that will host the schema representation
 # Most likely you just need to replace Remi-Gau in the following line by your github username
-remote_repo = 'https://raw.githubusercontent.com/Remi-Gau/schema-standardization/'
+REMOTE_REPO = 'https://raw.githubusercontent.com/Remi-Gau/schema-standardization/'
 
 # to which branch of schema-standardization the user interface will be pointed to
 # In the end the cobidas-UI repository will be reading the schema from the URL that that
-# starts with: remote_repo + branch_name
-branch_name  = 'neurovault'
+# starts with: REMOTE_REPO + BRANCH_NAME
+BRANCH_NAME = 'neurovault'
 
-
-ReproNim_repo = 'https://raw.githubusercontent.com/ReproNim/schema-standardization/master/'
+REPRONIM_REPO = 'https://raw.githubusercontent.com/ReproNim/schema-standardization/master/'
 
 
 ## -----------------------------------------------------------------------------
 ## -----------------------------------------------------------------------------
 ## -----------------------------------------------------------------------------
-
-import json
-import os
-import csv
-
 
 # activity set names
-activity_set_schema_filename = 'cobidas_schema.jsonld'
-activity_set_context_filename = 'cobidas_context.jsonld'
-activity_set_folder_name = 'cobidas'
+ACTIVITY_SET_SCHEMA_FILENAME = 'cobidas_schema.jsonld'
+ACTIVITY_SET_CONTEXT_FILENAME = 'cobidas_context.jsonld'
+ACTIVITY_SET_FOLDER_NAME = 'cobidas'
 
 
-# version
-version = '0.0.1'
+# VERSION
+VERSION = '0.0.1'
 
 # make output directories
-if not os.path.exists(os.path.join(output_dir, 'activity-sets', activity_set_folder_name)):
-    os.makedirs(os.path.join(output_dir, 'activity-sets', activity_set_folder_name))
-
+if not os.path.exists(os.path.join(OUTPUT_DIR, 'activity-sets', ACTIVITY_SET_FOLDER_NAME)):
+    os.makedirs(os.path.join(OUTPUT_DIR, 'activity-sets', ACTIVITY_SET_FOLDER_NAME))
 
 
 # define the activity set neurovault_schema.jsonld
-nv_set_schema_json = {
-    '@context': [ ReproNim_repo + 'contexts/generic',
-        remote_repo + branch_name + '/activity-sets/' + activity_set_folder_name + '/' + activity_set_context_filename
-    ],
-    '@type': ReproNim_repo + 'schemas/ActivitySet',
+NV_SET_SCHEMA_JSON = {
+    '@context': [REPRONIM_REPO + 'contexts/generic',
+                 REMOTE_REPO + BRANCH_NAME + '/activity-sets/'
+                 + ACTIVITY_SET_FOLDER_NAME + '/'
+                 + ACTIVITY_SET_CONTEXT_FILENAME
+                ],
+    '@type': 'reproschema:ActivitySet',
     '@id': 'cobidas_schema',
-    'prefLabel': 'neurovault as a COBIDAS POC',
-    'altLabel': 'neurovault_COBIDAS_POC',
-    'description': 'neurovault as a COBIDAS checklist proof of concept',
-    'schemaVersion': version,
-    'version': version,
-    'about': remote_repo + branch_name + '/activity-sets/' + '/README.md',
+    'skos:prefLabel': 'neurovault as a COBIDAS POC',
+    'skos:altLabel': 'neurovault_COBIDAS_POC',
+    'schema:description': 'neurovault as a COBIDAS checklist proof of concept',
+    'schema:schemaVERSION': VERSION,
+    'schema:VERSION': VERSION,
+    'about': REMOTE_REPO + BRANCH_NAME + '/activity-sets/' + '/README.md',
     'variableMap': [],
     'ui': {
         'order': [],
@@ -77,69 +77,77 @@ nv_set_schema_json = {
 }
 
 # define the activity set neurovault_context.jsonld
-nv_set_context_json = {
+NV_SET_CONTEXT_JSON = {
     '@context': {
-        '@version': 1.1,
-        'activity_path': remote_repo + branch_name + '/activities/',
+        '@VERSION': 1.1,
+        'activity_path': REMOTE_REPO + BRANCH_NAME + '/activities/',
     }
 }
 
 
-Section = ''
+SECTION = ''
 # loop through rows of the csv file and create corresponding jsonld for each item
-with open(input_file, 'r') as csvfile:
-    nv_metadata = csv.reader(csvfile)
-    for row in nv_metadata:
+with open(INPUT_FILE, 'r') as csvfile:
+    NV_METADATA = csv.reader(csvfile)
+    for row in NV_METADATA:
 
         # to skip the header
-        if row[2]!='Item':
+        if row[2] != 'Item':
 
-            # detect if this is a new section if so it will create a new activity
-            if row[1]!=Section:
+            # detect if this is a new SECTION if so it will create a new activity
+            if row[1] != SECTION:
 
-                # update section name
-                Section=row[1]
+                # update SECTION name
+                SECTION = row[1]
 
-                # where the items of this section will be stored
-                activity_folder_name = 'Neurovault_' + Section
+                # where the items of this SECTION will be stored
+                activity_folder_name = 'Neurovault_' + SECTION
 
-                # names of this section schema and its corresponding jsonld files
-                activity_schema_name = 'Neurovault_' + Section + '_schema'
+                # names of this SECTION schema and its corresponding jsonld files
+                activity_schema_name = 'Neurovault_' + SECTION + '_schema'
 
                 activity_schema_filename = activity_schema_name + '.jsonld'
 
-                activity_context_filename = 'Neurovault_' + Section + '_context.jsonld'
+                activity_context_filename = 'Neurovault_' + SECTION + '_context.jsonld'
 
 
                 print(activity_schema_name)
 
-                # create dir for this section
-                if not os.path.exists(os.path.join(output_dir, 'activities', activity_folder_name)):
-                    os.makedirs(os.path.join(output_dir, 'activities', activity_folder_name))
+                # create dir for this SECTION
+                if not os.path.exists(os.path.join(OUTPUT_DIR, 'activities',
+                                                   activity_folder_name)):
+                    os.makedirs(os.path.join(OUTPUT_DIR, 'activities', activity_folder_name))
 
-                if not os.path.exists(os.path.join(output_dir, 'activities', activity_folder_name, 'items')):
-                    os.makedirs(os.path.join(output_dir, 'activities', activity_folder_name, 'items'))
+                if not os.path.exists(os.path.join(OUTPUT_DIR, 'activities',
+                                                   activity_folder_name, 'items')):
+                    os.makedirs(os.path.join(OUTPUT_DIR, 'activities',
+                                             activity_folder_name, 'items'))
 
 
-                # define the base json content for the activity: neurovault_schema.jsonld neurovault_context.jsonld
+                # define the base json content for the activity:
+                # neurovault_schema.jsonld
+                # neurovault_context.jsonld
                 nv_context_json = {
                     '@context': {
-                        '@version': 1.1,
-                        'item_path': remote_repo + branch_name + '/activities/' + activity_folder_name + '/items/',
+                        '@VERSION': 1.1,
+                        'item_path': REMOTE_REPO + BRANCH_NAME + '/activities/'
+                                     + activity_folder_name + '/items/',
                         }
                     }
 
                 nv_schema_json = {
-                    '@context': [ ReproNim_repo + 'contexts/generic',
-                        remote_repo + branch_name + '/activities/' + activity_folder_name + '/' + activity_context_filename
-                    ],
-                    '@type': ReproNim_repo + 'schemas/Activity',
+                    '@context': [REPRONIM_REPO + 'contexts/generic',
+                                 REMOTE_REPO + BRANCH_NAME + '/activities/'
+                                 + activity_folder_name + '/'
+                                 + activity_context_filename
+                                ],
+                    '@type': 'reproschema:Activity',
                     '@id': activity_schema_name,
-                    'prefLabel': 'COBIDAS design checklist',
-                    'altLabel': 'cobidas_design_schema',
-                    'description': 'COBIDAS design checklist schema',
-                    'schemaVersion': version,
-                    'version': version,
+                    'skos:prefLabel': 'COBIDAS design checklist',
+                    'skos:altLabel': 'cobidas_design_schema',
+                    'schema:description': 'COBIDAS design checklist schema',
+                    'schema:schemaVERSION': VERSION,
+                    'schema:VERSION': VERSION,
                     'variableMap': [],
                     'preamble': 'How did you design/analyse your study?',
                     'ui': {
@@ -150,16 +158,16 @@ with open(input_file, 'r') as csvfile:
                     }
                 }
 
-                # update the json content of the activity set schema and context wrt this new activity
-                nv_set_schema_json['variableMap'].append(
-                    {'variableName': activity_schema_name,'isAbout': activity_schema_name}
+                # update the json content of activity set schema and context wrt this new activity
+                NV_SET_SCHEMA_JSON['variableMap'].append(
+                    {'variableName': activity_schema_name, 'isAbout': activity_schema_name}
                     )
 
-                nv_set_schema_json['ui']['order'].append(activity_schema_name)
-                nv_set_schema_json['ui']['visibility'][activity_schema_name] = True
-                nv_set_schema_json['ui']['activity_display_name'][activity_schema_name] = 'Neurovault - ' + Section
+                NV_SET_SCHEMA_JSON['ui']['order'].append(activity_schema_name)
+                NV_SET_SCHEMA_JSON['ui']['visibility'][activity_schema_name] = True
+                NV_SET_SCHEMA_JSON['ui']['activity_display_name'][activity_schema_name] = 'Neurovault - ' + SECTION
 
-                nv_set_context_json['@context'][activity_schema_name] = {
+                NV_SET_CONTEXT_JSON['@context'][activity_schema_name] = {
                     '@id': 'activity_path:' + activity_folder_name + '/' + activity_schema_filename,
                     '@type': '@id'
                     }
@@ -169,18 +177,18 @@ with open(input_file, 'r') as csvfile:
 
             # update the json content of the activity schema and context wrt this new item
             nv_context_json['@context'][row[2]] = {
-                '@id': 'item_path:'+row[2]+'.jsonld',
+                '@id': 'item_path:' + row[2] + '.jsonld',
                 '@type': '@id'
             }
 
             nv_schema_json['ui']['order'].append(row[2])
 
             nv_schema_json['variableMap'].append(
-                {'variableName': row[2],'isAbout': row[2]}
+                {'variableName': row[2], 'isAbout': row[2]}
                 )
 
             # branchic logic: visibility
-            if row[6]=='1':
+            if row[6] == '1':
                 nv_schema_json['ui']['visibility'][row[2]] = True
             else:
                 nv_schema_json['ui']['visibility'][row[2]] = row[6] + ' === 1'
@@ -188,30 +196,34 @@ with open(input_file, 'r') as csvfile:
 
 
             # save activity jsonld with every new item
-            with open(os.path.join(output_dir, 'activities', activity_folder_name, activity_schema_filename), 'w') as ff:
+            with open(os.path.join(OUTPUT_DIR, 'activities', activity_folder_name,
+                                   activity_schema_filename), 'w') as ff:
                 json.dump(nv_schema_json, ff, sort_keys=False, indent=4)
 
-            with open(os.path.join(output_dir, 'activities', activity_folder_name, activity_context_filename), 'w') as ff:
+            with open(os.path.join(OUTPUT_DIR, 'activities', activity_folder_name,
+                                   activity_context_filename), 'w') as ff:
                 json.dump(nv_context_json, ff, sort_keys=False, indent=4)
 
 
             # define jsonld for this item
             item_json = {
-                '@context': [ ReproNim_repo + 'contexts/generic',
-                    remote_repo + branch_name + '/activities/' + activity_folder_name + '/' + activity_context_filename
-                ],
-                '@type': ReproNim_repo + 'schemas/Field',
+                '@context': [REPRONIM_REPO + 'contexts/generic',
+                             REMOTE_REPO + BRANCH_NAME + '/activities/'
+                             + activity_folder_name + '/'
+                             + activity_context_filename
+                            ],
+                '@type': 'reproschema:Field',
                 '@id': row[2],
-                'prefLabel': row[2],
-                'altLabel': row[2],
-                'description': row[2],
-                'schemaVersion': version,
-                'version': version,
+                'skos:prefLabel': row[2],
+                'skos:altLabel': row[2],
+                'schema:description': row[2],
+                'schema:schemaVERSION': VERSION,
+                'schema:VERSION': VERSION,
                 'question': row[3],
             }
 
             # now we define the answers for this item
-            if row[4]=='Boolean':
+            if row[4] == 'Boolean':
 
                 item_json['ui'] = {
                     'inputType': 'radio'
@@ -223,20 +235,20 @@ with open(input_file, 'r') as csvfile:
                     'maxValue': 1,
                     'choices': [
                         {
-                        '@type': 'Boolean',
-                        'name': 'no',
-                        'value': 0,
+                            '@type': 'Boolean',
+                            'name': 'no',
+                            'value': 0,
                         },
                         {
-                        '@type': 'Boolean',
-                        'name': 'yes',
-                        'value': 1,
+                            '@type': 'Boolean',
+                            'name': 'yes',
+                            'value': 1,
                         }
                     ]
                 }
 
             # if we have multiple choices
-            elif row[4][0]=='[':
+            elif row[4][0] == '[':
 
                 # we get all the possible options and add them to the possible responses
                 options = row[4][1:-2].replace("'", "").split(',')
@@ -256,33 +268,33 @@ with open(input_file, 'r') as csvfile:
                 for i, opt in enumerate(options):
 
                     item_json['responseOptions']['choices'].append({
-                            'name': {'en': opt},
-                            'value': i,
-                            })
+                        'name': {'en': opt},
+                        'value': i,
+                        })
 
 
             # response is some integer
-            elif row[4]=='int':
-                    item_json['ui'] = {
-                        'inputType': 'number'
-                    }
-                    item_json['responseOptions'] = {
-                        'type': 'xsd:integer',
-                    }
+            elif row[4] == 'int':
+                item_json['ui'] = {
+                    'inputType': 'number'
+                }
+                item_json['responseOptions'] = {
+                    'type': 'xsd:integer',
+                }
 
 
             # response is some float
-            elif row[4]=='float':
-                        item_json['ui'] = {
-                            'inputType': 'float'
-                        }
-                        item_json['responseOptions'] = {
-                            'type': 'xsd:float',
-                        }
+            elif row[4] == 'float':
+                item_json['ui'] = {
+                    'inputType': 'float'
+                }
+                item_json['responseOptions'] = {
+                    'type': 'xsd:float',
+                }
 
 
             # input requires some typed answer
-            elif row[4]=='char':
+            elif row[4] == 'char':
                 item_json['ui'] = {
                     'inputType': 'text'
                     }
@@ -300,13 +312,16 @@ with open(input_file, 'r') as csvfile:
 
 
             # write item jsonld
-            with open(os.path.join(output_dir, 'activities', activity_folder_name, 'items', row[2] + '.jsonld'), 'w') as ff:
+            with open(os.path.join(OUTPUT_DIR, 'activities', activity_folder_name, 'items', row[2]
+                                   + '.jsonld'), 'w') as ff:
                 json.dump(item_json, ff, sort_keys=False, indent=4)
 
 
 # write activity set jsonld
-with open(os.path.join(output_dir, 'activity-sets', activity_set_folder_name, activity_set_schema_filename), 'w') as ff:
-    json.dump(nv_set_schema_json, ff, sort_keys=False, indent=4)
+with open(os.path.join(OUTPUT_DIR, 'activity-sets', ACTIVITY_SET_FOLDER_NAME,
+                       ACTIVITY_SET_SCHEMA_FILENAME), 'w') as ff:
+    json.dump(NV_SET_SCHEMA_JSON, ff, sort_keys=False, indent=4)
 
-with open(os.path.join(output_dir, 'activity-sets', activity_set_folder_name, activity_set_context_filename), 'w') as ff:
-    json.dump(nv_set_context_json, ff, sort_keys=False, indent=4)
+with open(os.path.join(OUTPUT_DIR, 'activity-sets', ACTIVITY_SET_FOLDER_NAME,
+                       ACTIVITY_SET_CONTEXT_FILENAME), 'w') as ff:
+    json.dump(NV_SET_CONTEXT_JSON, ff, sort_keys=False, indent=4)
