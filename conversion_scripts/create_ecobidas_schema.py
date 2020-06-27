0020517f5b4c04e13bf61887b18a816110404bf0
Remi Gau <remi_gau@hotmail.com>
2020-06-27 20:16:18 +0200

    shorten variable names

--- conversion_scripts/create_ecobidas_schema.py
+++ conversion_scripts/create_ecobidas_schema.py
@@ -13,23 +13,23 @@ import csv
 ## -----------------------------------------------------------------------------
 # modify the following lines to match your needs
 
-# where the metadata from neurovault are described. It is in xlsx folder of this repos
+# where the metadata from neurovault are described. It is in xlsx dir of this repos
 # but it can also be downloaded from here:
 # https://github.com/NeuroVault/NeuroVault/blob/master/scripts/metadata_neurovault.csv
 INPUT_FILE = '/home/remi/github/COBIDAS_chckls/xlsx/metadata_neurovault.csv'
 
 # where the files will be written on your machine: the local repository
 # corresponding to the remote where of the reproschema will be hosted
-OUTPUT_DIR = '/home/remi/github/reproschema'
+OUTPUT_DIR = '/home/remi/github/COBIDAS_chckls'
 
 # Placeholder to insert in all instances of the remote repo that will host the schema representation
 # Most likely you just need to replace Remi-Gau in the following line by your github username
-REMOTE_REPO = 'https://raw.githubusercontent.com/Remi-Gau/reproschema/'
+REMOTE_REPO = 'https://raw.githubusercontent.com/Remi-Gau/COBIDAS_chckls/'
 
 # to which branch of reproschema the user interface will be pointed to
 # In the end the cobidas-UI repository will be reading the schema from the URL that that
-# starts with: REMOTE_REPO + BRANCH_NAME
-BRANCH_NAME = 'remi-test'
+# starts with: REMOTE_REPO + BRANCH
+BRANCH = 'neurovault'
 
 REPRONIM_REPO = 'https://raw.githubusercontent.com/ReproNim/reproschema/master/'
 
@@ -39,56 +39,57 @@ REPRONIM_REPO = 'https://raw.githubusercontent.com/ReproNim/reproschema/master/'
 ## -----------------------------------------------------------------------------
 
 # activity set names
-PROTOCOL_NAME = 'cobidas_'
+PROTOCOL = 'neurovault_'
 
 # CSV column
-SECTION_COLUMN = 1
-ITEM_COLUMN = 2
-RESPONSE_TYPE_COLUMN = 4
-QUESTION_COLUMN = 3
-VISIBILITY_COLUMN = 6
+SECTION_COL = 1
+ITEM_COL = 2
+QUESTION_COL = 3
+RESPONSE_TYPE_COL = 4
+CHOICE_COL = 5
+MANDATORY_COL = 6
+VISIBILITY_COL = 7
 
 # protocol names
-PROTOCOL_SCHEMA_FILENAME = PROTOCOL_NAME + 'schema'
-PROTOCOL_CONTEXT_FILENAME = PROTOCOL_NAME +  'context'
-PROTOCOL_FOLDER_NAME = PROTOCOL_NAME[0:-1]
+PROTOCOL_SCHEMA_FILE = PROTOCOL + 'schema'
+PROTOCOL_CONTEXT_FILE = PROTOCOL +  'context'
+PROTOCOL_DIR = PROTOCOL[0:-1]
 
 # VERSION
 VERSION = '0.0.1'
 
 # make output directories
-if not os.path.exists(os.path.join(OUTPUT_DIR, 'protocols', PROTOCOL_FOLDER_NAME)):
-    os.makedirs(os.path.join(OUTPUT_DIR, 'protocols', PROTOCOL_FOLDER_NAME))
+if not os.path.exists(os.path.join(OUTPUT_DIR, 'protocols', PROTOCOL_DIR)):
+    os.makedirs(os.path.join(OUTPUT_DIR, 'protocols', PROTOCOL_DIR))
 
 
-# define the activity set neurovault_schema.jsonld
+# define the jsonld for the schema protocol
 PROTOCOL_SCHEMA_JSON = {
     '@context': [REPRONIM_REPO + 'contexts/generic',
-                 REMOTE_REPO + BRANCH_NAME + '/protocols/'
-                 + PROTOCOL_FOLDER_NAME + '/'
-                 + PROTOCOL_CONTEXT_FILENAME
+                 REMOTE_REPO + BRANCH + '/protocols/'
+                 + PROTOCOL_DIR + '/'
+                 + PROTOCOL_CONTEXT_FILE
                 ],
     '@type': 'reproschema:Protocol',
-    '@id': PROTOCOL_SCHEMA_FILENAME,
-    'skos:prefLabel': PROTOCOL_SCHEMA_FILENAME,
-    'schema:description': PROTOCOL_SCHEMA_FILENAME,
+    '@id': PROTOCOL_SCHEMA_FILE,
+    'prefLabel': PROTOCOL_SCHEMA_FILE,
+    'schema:description': PROTOCOL_SCHEMA_FILE,
     'schema:schemaVersion': VERSION,
     'schema:version': VERSION,
-    'landingPage': REMOTE_REPO + BRANCH_NAME + '/protocols/README.md',
-    'variableMap': [],
+    'landingPage': REMOTE_REPO + BRANCH + '/protocols/'+ PROTOCOL[0:-1] +'/README.md',
     'ui': {
-        'order': [],
+        'allow': ["autoAdvance", "allowExport"],
         'shuffle': False,
-        'activity_display_name': {},
-        'visibility': {}
+        'order': [],
+        'addProperties': [],
     }
 }
 
-# define the activity set neurovault_context.jsonld
+# define the jsonld for the context associated to this protocol
 PROTOCOL_CONTEXT_JSON = {
     '@context': {
         '@version': 1.1,
-        'activity_path': REMOTE_REPO + BRANCH_NAME + '/activities/',
+        'activity_path': REMOTE_REPO + BRANCH + '/activities/',
     }
 }
 
@@ -154,119 +155,111 @@ with open(INPUT_FILE, 'r') as csvfile:
     for row in PROTOCOL_METADATA:
 
         # to skip the header
-        if row[ITEM_COLUMN] != 'Item':
+        if row[ITEM_COL] != 'Item':
+
+            item_name = row[ITEM_COL]
+
+            question = row[QUESTION_COL]
 
             # detect if this is a new SECTION if so it will create a new activity
-            if row[SECTION_COLUMN] != SECTION:
+            if row[SECTION_COL] != SECTION:
 
                 # update SECTION name
-                SECTION = row[SECTION_COLUMN]
+                SECTION = row[SECTION_COL]
 
                 # where the items of this SECTION will be stored
-                activity_folder_name = PROTOCOL_NAME + SECTION
+                activity_dir = PROTOCOL + SECTION
 
                 # names of this SECTION schema and its corresponding jsonld files
-                activity_schema_name = PROTOCOL_NAME + SECTION + '_schema'
+                activity_schema_name = PROTOCOL + SECTION
 
-                activity_schema_filename = activity_schema_name
+                activity_schema_file = activity_schema_name + '_schema'
 
-                activity_context_filename = PROTOCOL_NAME + SECTION + '_context'
+                activity_context_file = PROTOCOL + SECTION + '_context'
 
 
                 print(activity_schema_name)
 
+
                 # create dir for this SECTION
                 if not os.path.exists(os.path.join(OUTPUT_DIR, 'activities',
-                                                   activity_folder_name)):
-                    os.makedirs(os.path.join(OUTPUT_DIR, 'activities', activity_folder_name))
+                                                   activity_dir)):
+                    os.makedirs(os.path.join(OUTPUT_DIR, 'activities', activity_dir))
 
                 if not os.path.exists(os.path.join(OUTPUT_DIR, 'activities',
-                                                   activity_folder_name, 'items')):
+                                                   activity_dir, 'items')):
                     os.makedirs(os.path.join(OUTPUT_DIR, 'activities',
-                                             activity_folder_name, 'items'))
+                                             activity_dir, 'items'))
 
 
                 activity_context_json, activity_schema_json = define_new_activity(REMOTE_REPO, BRANCH, activity_dir, REPRONIM_REPO, activity_context_file, activity_schema_name, PROTOCOL, SECTION, VERSION)
 
-                protocol_schema_json = {
-                    '@context': [REPRONIM_REPO + 'contexts/generic',
-                                 REMOTE_REPO + BRANCH_NAME + '/activities/'
-                                 + activity_folder_name + '/'
-                                 + activity_context_filename
-                                ],
-                    '@type': 'reproschema:Activity',
-                    '@id': activity_schema_name,
-                    'skos:prefLabel': PROTOCOL_NAME + SECTION,
-                    'schema:description': PROTOCOL_NAME + SECTION,
-                    'schema:schemaVersion': VERSION,
-                    'schema:version': VERSION,
-                    'variableMap': [],
-                    'preamble': ' ',
-                    'ui': {
-                        'order': [],
-                        'visibility': {},
-                        'shuffle': False,
-                        'allow': ["skipped"]
+
+                # update the content of the protool schema and context wrt this new activity
+                to_append_to_protocol_schema = {
+                    'variableName': activity_schema_name,
+                    'isAbout': activity_schema_name,
+                    "prefLabel": {
+                    "en": activity_schema_name
                     }
                 }
 
-                # update the json content of activity set schema and context wrt this new activity
-                PROTOCOL_SCHEMA_JSON['variableMap'].append(
-                    {'variableName': activity_schema_name, 'isAbout': activity_schema_name}
-                    )
-
                 PROTOCOL_SCHEMA_JSON['ui']['order'].append(activity_schema_name)
-                PROTOCOL_SCHEMA_JSON['ui']['visibility'][activity_schema_name] = True
-                PROTOCOL_SCHEMA_JSON['ui']['activity_display_name'][activity_schema_name] = SECTION + '_schema' # TO DO: add space when upercase
+                PROTOCOL_SCHEMA_JSON['ui']['addProperties'].append(to_append_to_protocol_schema)
 
                 PROTOCOL_CONTEXT_JSON['@context'][activity_schema_name] = {
-                    '@id': 'activity_path:' + activity_folder_name + '/' + activity_schema_filename,
+                    '@id': 'activity_path:' + activity_dir + '/' + activity_schema_file,
                     '@type': '@id'
                     }
 
-            print('   ' + row[ITEM_COLUMN])
+
+            print('   ' + row[ITEM_COL])
 
 
             # update the json content of the activity schema and context wrt this new item
-            protocol_context_json['@context'][row[ITEM_COLUMN]] = {
-                '@id': 'item_path:' + row[ITEM_COLUMN],
+
+            # branchic logic: visibility
+            if row[VISIBILITY_COL] == '1':
+                visibility = True
+            else:
+                visibility = row[VISIBILITY_COL] + ' === 1'
+
+            to_append_to_activity_schema = {
+                'variableName': item_name,
+                'isAbout': item_name,
+                "isVis": visibility
+            }
+
+            activity_schema_json['ui']['order'].append(item_name)
+            activity_schema_json['ui']['addProperties'].append(to_append_to_activity_schema)
+
+            activity_context_json['@context'][item_name] = {
+                '@id': 'item_path:' + item_name,
                 '@type': '@id'
             }
 
-            protocol_schema_json['ui']['order'].append(row[ITEM_COLUMN])
-
-            protocol_schema_json['variableMap'].append(
-                {'variableName': row[ITEM_COLUMN], 'isAbout': row[ITEM_COLUMN]}
-                )
-
-            # branchic logic: visibility
-            if row[VISIBILITY_COLUMN] == '1':
-                protocol_schema_json['ui']['visibility'][row[ITEM_COLUMN]] = True
-            else:
-                protocol_schema_json['ui']['visibility'][row[ITEM_COLUMN]] = row[VISIBILITY_COLUMN] + ' === 1'
-
-
 
             # save activity jsonld with every new item
-            with open(os.path.join(OUTPUT_DIR, 'activities', activity_folder_name,
-                                   activity_schema_filename), 'w') as ff:
-                json.dump(protocol_schema_json, ff, sort_keys=False, indent=4)
+            with open(os.path.join(OUTPUT_DIR, 'activities', activity_dir,
+                                   activity_schema_file), 'w') as ff:
+                json.dump(activity_schema_json, ff, sort_keys=False, indent=4)
 
-            with open(os.path.join(OUTPUT_DIR, 'activities', activity_folder_name,
-                                   activity_context_filename), 'w') as ff:
-                json.dump(protocol_context_json, ff, sort_keys=False, indent=4)
+            with open(os.path.join(OUTPUT_DIR, 'activities', activity_dir,
+                                   activity_context_file), 'w') as ff:
+                json.dump(activity_context_json, ff, sort_keys=False, indent=4)
+
 
 
             # Create new item
             item_schema = define_new_item(REPRONIM_REPO, REMOTE_REPO, BRANCH, activity_dir, activity_context_file, item_name, question, VERSION)
 
             # now we define the answers for this item
-            if row[RESPONSE_TYPE_COLUMN]  == 'Boolean':
+            if row[RESPONSE_TYPE_COL]  == 'Boolean':
 
-                item_json['ui'] = {
+                item_schema['ui'] = {
                     'inputType': 'radio'
                 }
-                item_json['responseOptions'] = {
+                item_schema['responseOptions'] = {
                     '@type': 'xsd:anyURI',
                     'multipleChoice': False,
                     'minValue': 0,
@@ -286,16 +279,16 @@ with open(INPUT_FILE, 'r') as csvfile:
                 }
 
             # if we have multiple choices
-            elif row[RESPONSE_TYPE_COLUMN][0] == '[':
+            elif row[RESPONSE_TYPE_COL][0] == '[':
 
                 # we get all the possible options and add them to the possible responses
-                options = row[RESPONSE_TYPE_COLUMN][1:-2].replace("'", "").split(',')
+                options = row[RESPONSE_TYPE_COL][1:-2].replace("'", "").split(',')
 
-                item_json['ui'] = {
+                item_schema['ui'] = {
                     'inputType': 'select'
                     }
 
-                item_json['responseOptions'] = {
+                item_schema['responseOptions'] = {
                     "dataType": "xsd:string",
                     'multipleChoice': True,
                     'minValue': 0,
@@ -305,60 +298,60 @@ with open(INPUT_FILE, 'r') as csvfile:
 
                 for i, opt in enumerate(options):
 
-                    item_json['responseOptions']['choices'].append({
+                    item_schema['responseOptions']['choices'].append({
                         'name': {'en': opt},
                         'value': i,
                         })
 
 
             # response is some integer
-            elif row[RESPONSE_TYPE_COLUMN]  == 'int':
-                item_json['ui'] = {
+            elif row[RESPONSE_TYPE_COL]  == 'int':
+                item_schema['ui'] = {
                     'inputType': 'number'
                 }
-                item_json['responseOptions'] = {
+                item_schema['responseOptions'] = {
                     'type': 'xsd:integer',
                 }
 
 
             # response is some float
-            elif row[RESPONSE_TYPE_COLUMN]  == 'float':
-                item_json['ui'] = {
+            elif row[RESPONSE_TYPE_COL]  == 'float':
+                item_schema['ui'] = {
                     'inputType': 'float'
                 }
-                item_json['responseOptions'] = {
+                item_schema['responseOptions'] = {
                     'type': 'xsd:float',
                 }
 
 
             # input requires some typed answer
-            elif row[RESPONSE_TYPE_COLUMN]  == 'char':
-                item_json['ui'] = {
+            elif row[RESPONSE_TYPE_COL]  == 'char':
+                item_schema['ui'] = {
                     'inputType': 'text'
                     }
-                item_json['responseOptions'] = {
+                item_schema['responseOptions'] = {
                     'type': 'xsd:string'
                     }
 
             else:
-                item_json['ui'] = {
+                item_schema['ui'] = {
                     'inputType': 'text'
                     }
-                item_json['responseOptions'] = {
+                item_schema['responseOptions'] = {
                     'type': 'xsd:string'
                     }
 
 
-            # write item jsonld
-            with open(os.path.join(OUTPUT_DIR, 'activities', activity_folder_name, 'items', row[ITEM_COLUMN]), 'w') as ff:
-                json.dump(item_json, ff, sort_keys=False, indent=4)
+            # write item schema
+            with open(os.path.join(OUTPUT_DIR, 'activities', activity_dir, 'items', row[ITEM_COL]), 'w') as ff:
+                json.dump(item_schema, ff, sort_keys=False, indent=4)
 
 
 # write activity set jsonld
-with open(os.path.join(OUTPUT_DIR, 'protocols', PROTOCOL_FOLDER_NAME,
-                       PROTOCOL_SCHEMA_FILENAME), 'w') as ff:
+with open(os.path.join(OUTPUT_DIR, 'protocols', PROTOCOL_DIR,
+                       PROTOCOL_SCHEMA_FILE), 'w') as ff:
     json.dump(PROTOCOL_SCHEMA_JSON, ff, sort_keys=False, indent=4)
 
-with open(os.path.join(OUTPUT_DIR, 'protocols', PROTOCOL_FOLDER_NAME,
-                       PROTOCOL_CONTEXT_FILENAME), 'w') as ff:
+with open(os.path.join(OUTPUT_DIR, 'protocols', PROTOCOL_DIR,
+                       PROTOCOL_CONTEXT_FILE), 'w') as ff:
     json.dump(PROTOCOL_CONTEXT_JSON, ff, sort_keys=False, indent=4)
