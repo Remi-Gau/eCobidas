00e15a573ae9c92164508dfa1f4f429e8856af6d
Remi Gau <remi_gau@hotmail.com>
2021-06-05 07:51:50 +0200

    create command line interface

--- python/conversion/convert_csv_to_schema.py
+++ python/conversion/convert_csv_to_schema.py
@@ -1,71 +1,88 @@
+import click
 from create_schema import create_schema
-
-# schema_to_create = ["mri_all_sequences" "participants" "behavior" "neurovault", "pet", "mri", "eyetracker" "artemis" "reexec_nimg"]
-
-schema_to_create = ["neurovault"]
 
 # -----------------------------------------------------------------------------
 #                                   PARAMETERS
 # -----------------------------------------------------------------------------
 # modify the following lines to match your needs
 
-# OUTPUT_DIR ----------------------------------------
+# schema_to_create = [
+#     "mri_all_sequences",
+#     "participants",
+#     "behavior",
+#     "neurovault",
+#     "pet",
+#     "mri",
+#     "eyetracker",
+#     "artemis",
+#     "reexec_nimg",
+# ]
+
+schema_to_create = ["neurovault"]
+
+# output_dir ----------------------------------------
 # where the files will be written on your machine: the local repository
 # corresponding to the remote where of the reproschema will be hosted
 
-OUTPUT_DIR = "/home/remi/github/cobidas_chckls"
-# OUTPUT_DIR = "/home/remi/github/cobidas-PET"
-# OUTPUT_DIR = "/home/remi/github/cobidas"
-# OUTPUT_DIR = "/home/remi/github/reexecute_nimg_checklist/"
+output_dir = "/home/remi/github/cobidas_chckls"
 
-# REMOTE_REPO ----------------------------------------
+# output_dir = "/home/remi/github/cobidas-PET"
+# output_dir = "/home/remi/github/cobidas"
+# output_dir = "/home/remi/github/reexecute_nimg_checklist/"
+
+# repo ----------------------------------------
 # Placeholder of the remote repo that will host the schema representation.
 # Most likely you just need to replace Remi-Gau in the following line by your
 # github username
 
-REMOTE_REPO = "https://raw.githubusercontent.com/Remi-Gau/eCobidas/"
-# REMOTE_REPO = "https://raw.githubusercontent.com/Remi-Gau/cobidas-PET/"
-# REMOTE_REPO = "https://raw.githubusercontent.com/ohbm/cobidas/"
-# REMOTE_REPO = "https://raw.githubusercontent.com/Remi-Gau/reexecute_nimg_checklist/"
+repo = "https://raw.githubusercontent.com/Remi-Gau/eCobidas/"
+# repo = "https://raw.githubusercontent.com/Remi-Gau/cobidas-PET/"
+# repo = "https://raw.githubusercontent.com/ohbm/cobidas/"
+# repo = "https://raw.githubusercontent.com/Remi-Gau/reexecute_nimg_checklist/"
 
-BRANCH = "master"
-
-# -----------------------------------------------------------------------------
-#                                   RUN
-# -----------------------------------------------------------------------------
+branch = "master"
 
 
-for schema in schema_to_create:
+@click.command()
+@click.option(
+    "--schema_to_create", default=schema_to_create, help="Name of the schema to create."
+)
+@click.option("--output_dir", default=output_dir, help="Where to output stuff.")
+@click.option("--repo", default=output_dir, help="repo")
+@click.option("--branch", default=output_dir, help="branch")
+def convert_to_schema(schema_to_create, output_dir, repo, branch):
 
-    protocol = create_schema(schema, OUTPUT_DIR)
+    for schema in schema_to_create:
 
-    s = "/"
+        protocol = create_schema(schema, output_dir)
 
-    print(
-        "\n\n"
-        + "---------------------------------------------------------------"
-        + "\nYou can view this protocol here:\n"
-        + "https://www.repronim.org/reproschema-ui/#/?url="
-        + s.join(
-            [
-                REMOTE_REPO,
-                BRANCH,
-                "protocols",
-                protocol.dir,
-                protocol.get_filename(),
-            ]
+        s = "/"
+
+        print(
+            "\n\n"
+            + "---------------------------------------------------------------"
+            + "\nYou can view this protocol here:\n"
+            + "https://www.repronim.org/reproschema-ui/#/?url="
+            + s.join(
+                [
+                    repo,
+                    branch,
+                    "protocols",
+                    protocol.dir,
+                    protocol.get_filename(),
+                ]
+            )
+            + "\n"
+            + "--------------------------------------------------------------"
+            + "\n"
+            + "https://www.repronim.org/reproschema-ui/#/?url=url-to-protocol-schema"
+            + "\n"
+            + "https://www.repronim.org/reproschema-ui/#/activities/0?url=url-to-activity-schema"
+            + "\n"
+            + "--------------------------------------------------------------"
+            + "\n\n",
         )
-        + "\n"
-        + "--------------------------------------------------------------"
-        + "\n"
-        + "https://www.repronim.org/reproschema-ui/#/?url=url-to-protocol-schema"
-        + "\n"
-        + "https://www.repronim.org/reproschema-ui/#/activities/0?url=url-to-activity-schema"
-        + "\n"
-        + "--------------------------------------------------------------"
-        + "\n\n",
-    )
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    convert_to_schema()
