e5c6a96443332274de4b9f8044b2a0456fcca440
Remi Gau <remi_gau@hotmail.com>
2021-06-05 12:55:06 +0200

    update tests

--- python/conversion/tests/data/README.md
+++ python/conversion/tests/data/README.md
@@ -2,3 +2,7 @@
 
 -   template data to test against
 -   adapted from the demo protocol of reproschema
+
+Link to view:
+
+https://www.repronim.org/reproschema-ui/#/?url=https://raw.githubusercontent.com/Remi-Gau/eCobidas/remi-dev/python/conversion/tests/data/protocols/test/protocol_schema
--- python/conversion/tests/test_convert_to_schema.py
+++ python/conversion/tests/test_convert_to_schema.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys, os, json
+
 
 from ..create_schema import convert_to_schema
 
@@ -13,3 +14,15 @@ def test_convert_to_schema():
     repo = "https://raw.githubusercontent.com/Remi-Gau/eCobidas/"
 
     convert_to_schema(schema_to_create, output_dir, repo)
+
+    output_file = os.path.join(output_dir, "protocols", "test", "test_schema")
+
+    with open(output_file, "r") as ff:
+        protocol_content = json.load(ff)
+
+    data_file = os.path.join(myPath, "data", "protocols", "test", "test_schema")
+
+    with open(data_file, "r") as ff:
+        expected = json.load(ff)
+
+    assert protocol_content == expected
