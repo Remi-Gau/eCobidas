44ff76f2b8f98dba63cfc2980fede8a4fd26c92c
Remi Gau <remi_gau@hotmail.com>
2021-06-19 10:53:26 +0200

    tmp awk

--- tmp.sh
+++ tmp.sh
@@ -2,16 +2,34 @@
 
 schema="core"
 
-IDs=($(awk 'BEGIN{pat="^core"} $0 ~ pat{print $4 }' inputs/csv/spreadsheet_google_id.tsv))
+csv_folder="./inputs/csv/"
+file="spreadsheet_google_id.tsv"
+input_file=$csv_folder$file
 
-len=${#IDs[@]}
-echo $len
+# output with awk intos arrays and will loop over google ids to download
+google_IDs=($(     awk -v schema=$schema 'BEGIN{pat="^"schema } $0 ~ pat{print $4}' $input_file))
+subfolder=($(      awk -v schema=$schema 'BEGIN{pat="^"schema } $0 ~ pat{print $2}' $input_file))
+output_filename=($(awk -v schema=$schema 'BEGIN{pat="^"schema } $0 ~ pat{print $3}' $input_file))
+
+len=${#google_IDs[@]}
+
+printf "\nSTART\n\n"
 
 for (( i=0; i<$len; i++ ))
 do
 
-echo $i
-echo "${IDs[$i]}"
+    echo "Downloading the ${subfolder[$i]} ${output_filename[$i]} spreadsheet to ${subfolder[$i]}/${output_filename[$i]}"
+    echo Google ID: "${google_IDs[$i]}"
+
+    ouput_folder="$csv_folder${subfolder[$i]}/"
+    mkdir -p $ouput_folder    
+
+    echo "https://docs.google.com/spreadsheets/d/${google_IDs[$i]}/export?format=tsv"
+
+    curl -L "https://docs.google.com/spreadsheets/d/${google_IDs[$i]}/export?format=tsv" \
+    -o $ouput_folder${output_filename[$i]}.tsv
+
+    printf "DONE\n\n"
 
 done
 
