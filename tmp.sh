635e856c18ae4bec5649480e313702f700addd7d
Remi Gau <remi_gau@hotmail.com>
2021-06-19 10:28:55 +0200

    tmp awk

---
+++ tmp.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+schema="core"
+
+IDs=($(awk '/core/ { print $4 }' inputs/csv/spreadsheet_google_id.tsv))
+
+len=${#IDs[@]}
+echo $len
+
+for (( i=0; i<$len; i++ ))
+do
+
+echo $i
+echo "${IDs[$i]}"
+
+done
+
