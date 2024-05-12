fe2e55a1af738cbc92d342d1801767b8d1bee7bd
Remi Gau <remi_gau@hotmail.com>
2024-05-12 20:48:29 +0200

    test landng page generation

--- ecobidas/generate_landing_page.py
+++ ecobidas/generate_landing_page.py
@@ -1,12 +1,15 @@
 """Generates a typical landing page in english for an app."""
 
-import os
+from pathlib import Path
 
 from ecobidas.template_manager import TemplateManager
 from ecobidas.utils import get_metatable
 
 
-def main():
+def main(output_dir: Path | None = None):
+    if output_dir is None:
+        output_dir = Path() / "output"
+
     TemplateManager.initialize()
 
     template = TemplateManager.env.get_template("landing_page.j2")
@@ -26,14 +29,14 @@ def main():
         if artemis:
             continue
 
-        dir = details["dir"].tolist()[0]
+        folder = details["dir"].tolist()[0]
         basename = details["basename"].to_string(index=False)
 
-        if dir == basename:
+        if folder == basename:
             basename = ""
 
         an_item = dict(
-            dir=dir,
+            folder=folder,
             basename=basename,
             app_link=details["app link"].tolist()[0],
             xls=details["link"].tolist()[0],
@@ -45,14 +48,14 @@ def main():
         if details["citation"].any():
             an_item["citation"] = details["citation"].tolist()[0]
 
-        if an_item["dir"] == "artemis":
+        if an_item["folder"] == "artemis":
             artemis = True
             an_item["basename"] = ""
 
         items.append(an_item)
 
     rendered_template = template.render(items=items)
-    with open(os.path.join("output", "landing_page.html"), "w") as out:
+    with open(output_dir / "landing_page.html", "w") as out:
         out.write(f"{rendered_template}")
 
 
--- tests/test_generate_tables.py
+++
@@ -1,8 +0,0 @@
-from ecobidas.app_list_table import main
-
-
-def test_generate_table(tmp_path):
-    main(tmp_path)
-
-    assert (tmp_path / "apps_table.md").exists()
-    assert (tmp_path / "preset_responses.md").exists()
