619e4e2ca98b80283673ca9bed8b31b53791936a
Remi Gau <remi_gau@hotmail.com>
2024-05-20 09:37:46 -0400

    fixes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -37,20 +37,15 @@ def test_get_output_dir(tmp_path):
 @pytest.mark.parametrize(
     "this_schema, filename",
     [
-        ("neurovault", "README_eCOBIDAS-en.md"),
-        ("test", "README_eCOBIDAS-en.md"),
-        ("pet", "README_PET-en.md"),
+        ("neurovault", "../README_eCOBIDAS-en.html"),
+        ("test", "../README_eCOBIDAS-en.md"),
+        ("pet", "../README_PET-en.md"),
     ],
 )
 def test_get_landing_page(this_schema, filename):
     schema_info = get_schema_info(this_schema)
     landing_page = get_landing_page(schema_info)
-
-    repo = "https://raw.githubusercontent.com/ohbm/cobidas_schema/master/landing_pages/"
-
-    expected = repo + filename
-
-    assert landing_page == expected
+    assert landing_page == filename
 
 
 def test_get_spreadsheets_info():
