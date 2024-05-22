3a477c76eabf05da50d31fd44b0a70372e555df9
Remi Gau <remi_gau@hotmail.com>
2024-05-22 11:56:38 -0400

    style items and deal with sliders items

--- ui/app.py
+++ ui/app.py
@@ -22,18 +22,9 @@ from wtforms import (
     SubmitField,
     TextAreaField,
 )
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, NumberRange
 
 LANG: str = "en"
-
-app: Flask = Flask(__name__)
-app.secret_key = "tO$&!|0wkamvVia0?n$NqIRVWOG"
-
-# Bootstrap-Flask requires this line
-bootstrap = Bootstrap5(app)
-
-# Flask-WTF requires this line
-csrf = CSRFProtect(app)
 
 
 @lru_cache
@@ -57,50 +48,6 @@ def get_landing_page(url: Path) -> None | dict[str, Any]:
         print(exc)
         data = None
     return data
-
-
-@app.route("/", methods=["GET", "POST"])
-def index() -> str:
-    return render_template("index.html")
-
-
-@app.route("/about")
-def about() -> str:
-    return render_template("about.html")
-
-
-@app.route("/new")
-def new() -> str:
-    return render_template("new_checklist.html")
-
-
-@app.route("/protocol/<protocol_name>", methods=["GET", "POST"])
-def protocol(protocol_name: str) -> str:
-
-    protocol_content = get_protocol(protocol_name)
-
-    landing_page_url = protocol_url(protocol_name).parent / protocol_content["landingPage"]["@id"]
-    landing_page = get_landing_page(landing_page_url)
-
-    activities = get_nav_bar_content(protocol_name)
-
-    return render_template(
-        "protocol.html",
-        protocol_pref_label=protocol_name,
-        protocol_preamble=protocol_content["preamble"][LANG],
-        activities=activities,
-        landing_page=Markup(landing_page),
-    )
-
-
-def sort_ui_properties(properties: list[dict[str, Any]], order: list[str]) -> list[dict[str, Any]]:
-    tmp = []
-    for i in order:
-        for prop in properties:
-            if prop["isAbout"] == i:
-                tmp.append(prop)
-                break
-    return tmp
 
 
 def get_nav_bar_content(
@@ -135,53 +82,6 @@ def get_nav_bar_content(
     return activities
 
 
-@app.route("/protocol/<protocol_name>/<activity_name>", methods=["GET", "POST"])
-def activity(protocol_name, activity_name) -> str:
-
-    protocol_name = escape(protocol_name)
-    activity_name = escape(activity_name)
-
-    protocol_content = get_protocol(protocol_name)
-    properties = protocol_content["ui"]["addProperties"]
-    for activity in properties:
-        if activity["variableName"] == activity_name:
-            is_about_activity = activity["isAbout"]
-            break
-    activity_file = protocol_url(protocol_name).parent / is_about_activity
-
-    with open(activity_file) as f:
-        activity = json.load(f)
-
-    activities = get_nav_bar_content(protocol_name, activity["prefLabel"][LANG])
-
-    items = get_items_for_activity(activity_file)
-
-    form = generate_form(items=items, prefix=activity_name)
-
-    if request.method == "POST" and form.validate_on_submit():
-        items = update_visibility(items, form)
-
-        form = generate_form(items, prefix=activity_name)
-
-        return render_template(
-            "protocol.html",
-            protocol_pref_label=protocol_name,
-            activity_pref_label=activity["prefLabel"][LANG],
-            activity_preamble=Markup(activity["preamble"][LANG]),
-            activities=activities,
-            form=form,
-        )
-
-    return render_template(
-        "protocol.html",
-        protocol_pref_label=protocol_name,
-        activity_pref_label=activity["prefLabel"][LANG],
-        activity_preamble=Markup(activity["preamble"][LANG]),
-        activities=activities,
-        form=form,
-    )
-
-
 def update_visibility(items: dict[str, Any], form):
     for item, values in items.items():
         isVis = values["isVis"]
@@ -191,11 +91,17 @@ def update_visibility(items: dict[str, Any], form):
                 app.logger.warning("More than 1 '=='")
             if len(isVis) < 2:
                 app.logger.warning(f"Unsupported JavaScript expression: {isVis}")
-            if len(isVis) == 2:
-                response = form[isVis[0]].data
-                value = items[isVis[0]]["choices"].get(response)
+            if len(isVis) == 2 and form[isVis[0]].data:
+                response = int(form[isVis[0]].data)
                 expected = int(isVis[1])
-                items[item]["visibility"] = value == expected
+                items[item]["visibility"] = response == expected
+    return items
+
+
+def update_format(items: dict[str, Any], form):
+    for i in items:
+        if form[i].data:
+            items[i]["is_answered"] = True
     return items
 
 
@@ -207,6 +113,10 @@ def generate_form(items=None, prefix=None):
     form = DyanmicForm
 
     for item_name, item in items.items():
+
+        render_kw = {"style": "border-color: red; border-width: 2px;"}
+        if item["is_answered"]:
+            render_kw = {"style": "border-color: green; border-width: 2px;"}
 
         validators = []
         if item["required"]:
@@ -247,6 +157,7 @@ def generate_form(items=None, prefix=None):
                     default=default,
                     _prefix=prefix,
                     format="%Y-%m-%d",
+                    render_kw=render_kw,
                 ),
             )
 
@@ -255,10 +166,8 @@ def generate_form(items=None, prefix=None):
             FieldType = StringField
             if input_type == "number":
                 FieldType = IntegerField
-                default = 0
             elif input_type in ["float"]:
                 FieldType = DecimalField
-                default = 0
             elif input_type == "textarea":
                 FieldType = TextAreaField
 
@@ -271,6 +180,35 @@ def generate_form(items=None, prefix=None):
                     description=item["description"],
                     default=default,
                     _prefix=prefix,
+                    render_kw=render_kw,
+                ),
+            )
+
+        elif input_type == "slider":
+
+            # FieldType = DecimalRangeField
+            # render_kw["value"]= "50.0"
+            # render_kw["min"]= "0.0"
+            # render_kw["max"]= "100.0"
+            # render_kw["step"]= "0.5"
+
+            min_label = float(item["choices"][0][1])
+            max_label = float(item["choices"][-1][1])
+
+            question += f" (min={min_label}, max={max_label})"
+
+            FieldType = DecimalField
+            validators.append(NumberRange(min=min_label, max=max_label))
+
+            setattr(
+                form,
+                item_name,
+                FieldType(
+                    Markup(question),
+                    validators=validators,
+                    description=item["description"],
+                    _prefix=prefix,
+                    render_kw=render_kw,
                 ),
             )
 
@@ -280,9 +218,9 @@ def generate_form(items=None, prefix=None):
 
             if is_multiple:
                 FieldType = SelectMultipleField
-            if input_type == "select":
+            elif input_type == "select":
                 FieldType = SelectField
-            elif input_type in ["radio", "slider"]:
+            elif input_type == "radio":
                 FieldType = RadioField
 
             setattr(
@@ -292,16 +230,18 @@ def generate_form(items=None, prefix=None):
                     Markup(question),
                     validators=validators,
                     description=item["description"],
-                    choices=[key for key in item["choices"]],
+                    choices=item["choices"],
                     _prefix=prefix,
+                    render_kw=render_kw,
                 ),
             )
 
-    setattr(form, "submit", SubmitField("Submit"))  # noqa B010
+    setattr(form, "submit", SubmitField("Save"))  # noqa B010
 
     return form()
 
 
+@lru_cache
 def get_items_for_activity(activity_file):
     with open(activity_file) as f:
         activity = json.load(f)
@@ -322,6 +262,7 @@ def get_items_for_activity(activity_file):
             "required": False,
             "isVis": item["isVis"],
             "choices": get_choices(item_data),
+            "is_answered": False,
         }
 
         tmp["description"] = item_data.get("description")
@@ -351,15 +292,117 @@ def get_items_for_activity(activity_file):
     return items
 
 
+def sort_ui_properties(properties: list[dict[str, Any]], order: list[str]) -> list[dict[str, Any]]:
+    tmp = []
+    for i in order:
+        for prop in properties:
+            if prop["isAbout"] == i:
+                tmp.append(prop)
+                break
+    return tmp
+
+
 def get_choices(item_data):
     try:
         choices = item_data["responseOptions"]["choices"]
         if isinstance(choices, str):
             choices = query_choices(choices)["choices"]
-        choices = {x["name"][LANG]: x["value"] for x in choices}
+        choices = [(x["value"], x["name"][LANG]) for x in choices]
     except KeyError:
         choices = {}
     return choices
+
+
+test_config = None
+
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY="tO$&!|0wkamvVia0?n$NqIRVWOG",
+    DATABASE=Path(app.instance_path) / "flaskr.sqlite",
+)
+
+if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile("config.py", silent=True)
+else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
+
+# ensure the instance folder exists
+Path(app.instance_path).mkdir(parents=True, exist_ok=True)
+
+# Bootstrap-Flask requires this line
+bootstrap = Bootstrap5(app)
+
+# Flask-WTF requires this line
+csrf = CSRFProtect(app)
+
+
+@app.route("/protocol/<protocol_name>", methods=["GET", "POST"])
+def protocol(protocol_name: str) -> str:
+
+    protocol_content = get_protocol(protocol_name)
+
+    landing_page_url = protocol_url(protocol_name).parent / protocol_content["landingPage"]["@id"]
+    landing_page = get_landing_page(landing_page_url)
+
+    activities = get_nav_bar_content(protocol_name)
+
+    return render_template(
+        "protocol.html",
+        protocol_pref_label=protocol_name,
+        protocol_preamble=protocol_content["preamble"][LANG],
+        activities=activities,
+        landing_page=Markup(landing_page),
+    )
+
+
+@app.route("/protocol/<protocol_name>/<activity_name>", methods=["GET", "POST"])
+def activity(protocol_name, activity_name) -> str:
+
+    protocol_name = escape(protocol_name)
+    activity_name = escape(activity_name)
+
+    protocol_content = get_protocol(protocol_name)
+    properties = protocol_content["ui"]["addProperties"]
+    for activity in properties:
+        if activity["variableName"] == activity_name:
+            is_about_activity = activity["isAbout"]
+            break
+    activity_file = protocol_url(protocol_name).parent / is_about_activity
+
+    with open(activity_file) as f:
+        activity = json.load(f)
+
+    activities = get_nav_bar_content(protocol_name, activity["prefLabel"][LANG])
+
+    items = get_items_for_activity(activity_file)
+
+    form = generate_form(items=items, prefix=activity_name)
+
+    if request.method == "POST" and form.is_submitted():
+        items = update_visibility(items, form)
+        items = update_format(items, form)
+        form = generate_form(items, prefix=activity_name)
+
+        return render_template(
+            "protocol.html",
+            protocol_pref_label=protocol_name,
+            activity_pref_label=activity["prefLabel"][LANG],
+            activity_preamble=Markup(activity["preamble"][LANG]),
+            activities=activities,
+            form=form,
+        )
+
+    return render_template(
+        "protocol.html",
+        protocol_pref_label=protocol_name,
+        activity_pref_label=activity["prefLabel"][LANG],
+        activity_preamble=Markup(activity["preamble"][LANG]),
+        activities=activities,
+        form=form,
+    )
 
 
 @app.errorhandler(404)
@@ -370,6 +413,21 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
+
+
+@app.route("/", methods=["GET", "POST"])
+def index() -> str:
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about() -> str:
+    return render_template("about.html")
+
+
+@app.route("/new")
+def new() -> str:
+    return render_template("new_checklist.html")
 
 
 # keep this as is
